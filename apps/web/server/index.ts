0916a7e3499181a5f02d8a3c03d56fb6ab5aa965
Anusha Arora <aroraanusha26@gmail.com>
2025-12-27 15:19:53 +0530

    adding models and users

--- apps/web/server/index.ts
+++ apps/web/server/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth"
 import { connectDB } from "./lib/db.config";
 
 dotenv.config();
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
-
+app.use("/auth", authRoutes);
 
 app.listen(PORT, async () => {
     console.log(`Server Starting : http://localhost:${PORT}`);
---
+++ apps/web/server/models/User.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id: string;                      
+  email: string;
+  name: string;
+  passwordHash: string;
+
+  authProvider: "local" | "google" | "github"; //we can include or exclude all this later
+
+  previousContentList: string[];   // storing content IDs or outputs
+
+  createdAt: string;               //ISO timestamp
+  lastLogin: string;               // ISO timestamp
+}
